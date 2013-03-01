163bb884886602b54dfe7a8c4b82646e315221bb
Adrian Kemp <adrian.kemp>
2013-03-01 01:32:11 -0500

    AdrianK
    Created empty sample project

---
+++ debug-logging-sample-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  debug-logging-sample-ios
+//
+//  Created by Adrian Kemp on 2013-03-01.
+//  Copyright (c) 2013 Adrian Kemp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ debug-logging-sample-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  debug-logging-sample-ios
+//
+//  Created by Adrian Kemp on 2013-03-01.
+//  Copyright (c) 2013 Adrian Kemp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
